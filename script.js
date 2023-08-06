03c34eb81b9337b15abd2a8437f6a73c2ca8cc23
Sayedmohebsadat460 <127274452+Sayedmohebsadat460@users.noreply.github.com>
2023-08-06 10:08:49 +0430

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed('.text', {
+    strings: ["FRONTEND DEVELOPER", "WEB DEVELOPER ", "YOUTUBER"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
+});
